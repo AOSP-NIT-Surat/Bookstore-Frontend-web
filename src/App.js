5736a48d284314a99d0ae3703d16f51717e68296
Pavithra Subramanian <pavithrasubramanian21@gmail.com>
2021-10-03 00:45:59 +0530

    added signin and signup page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./login.js";
+import SignUp from "./signup.js";
 
 function App() {
   return (
+  <Router>
     <div className="App">
-      Bookstore Frontend
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link id="nav" className="navbar-brand" to={"/login"}>BOOK STORE</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul id="nav1" className="navbar-nav ml-auto">
+              <li className="nav-item nav-right">
+                <Link id="nav" className="nav-link" to={"/login"}>Sign in</Link>
+              </li>
+              <li className="nav-item">
+                <Link id="nav" className="nav-link" to={"/signup"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="outer">
+        <div className="inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
     </div>
+    </Router>
   );
 }
 
