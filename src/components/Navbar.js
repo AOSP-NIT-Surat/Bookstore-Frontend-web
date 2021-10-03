688f807b334633e3b0c21162944306c2f81266af
SGR007 <sagarpardhi.test@gmail.com>
2021-10-03 20:56:43 +0530

    updated navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Bookstore Frontend
+        <Link className="navbar-brand ms-5" to="/">
+          <h2>Bookstore Frontend</h2>
         </Link>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler me-5"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarColor01"
@@ -20,15 +20,23 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarColor01">
+        <div className="collapse navbar-collapse me-5 ms-5" id="navbarColor01">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">
+            <li className="nav-item my-2 me-2">
+              <Link
+                to="/register"
+                className="btn btn-outline-light"
+                style={{ borderRadius: "5px" }}
+              >
                 Register
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
+            <li className="nav-item my-2 me-2">
+              <Link
+                to="/login"
+                className="btn btn-outline-light"
+                style={{ borderRadius: "5px" }}
+              >
                 Login
               </Link>
             </li>
