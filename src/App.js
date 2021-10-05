f112b7d220a1cf525dc24d5c9155491c873f99c6
Astel <astelthottankara@gmail.com>
2021-10-05 17:47:59 +0530

    refactor: move profile page and images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Layout from "./hocs/Layout";
-import Home from "./pages/Home";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Layout from './hocs/Layout';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Layout>
-          <Switch>
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </Layout>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Layout>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/profile" component={Profile} />
+					</Switch>
+				</Layout>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
