c47e16798fcaa3ddec739335e7562d8eb617f9a5
Mohit Agrawal <mohit27agrawal@gmail.com>
2021-10-05 12:32:33 +0530

    Mohit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import User from './Components/User';
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import User from "./Components/User";
+import Layout from "./hocs/Layout";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <User/>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/user"><User></User></Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
