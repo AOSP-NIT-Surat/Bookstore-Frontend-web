75f7b262cc44772af2cb4329ccc7a27c3b4b023c
Shubham Kr <72254047+shubhamkr83@users.noreply.github.com>
2021-10-06 12:24:42 +0530

    made 404 page

---
+++ src/pages/404Page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../index.css";
+
+const 404Page = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <p>Oops! Something is wrong.</p>
+            <a class="button" href="#"><i class="icon-home"></i> Go back in initial page, is better.</a>
+        </div>
+    )
+}
+
+export default 404;
